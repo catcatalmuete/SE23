996f70298378384edcd54c2ba12afc867e0573bd
Cat Almuete <ca2318@nyu.edu>
2023-09-18 15:47:01 -0400

    mkenv.sh

---
+++ mkenv.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+export PROJ_DIR=$(pwd)
+export PROJ_NAME=${PWD##*/}
+
+export VENV_NAME="$PROJ_NAME-venv"
+echo "Creating $VENV_NAME"
+
+python3 -m venv $VENV_NAME
+
+echo "export PYTHONPATH=$PROJ_DIR" >> $VENV_NAME/bin/activate
+
+echo "#/bin/bash
+source $VENV_NAME/bin/activate" > act.sh
+
+chmod 755 act.sh
